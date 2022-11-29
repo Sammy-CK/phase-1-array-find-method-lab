a89668e530bdb04a0e7f2f8148a4aa9e82d6bf88
Sammy-CK <sammycherono2696@gmail.com>
2022-11-29 12:29:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+
+function superbowlWin(record){
+ let determiner = (record.find(item => item.result === "W"))
+ if(determiner !== undefined){
+ return determiner.year;
+ }
+ return determiner
+}
